4225d8b7fb55e94970c7d0bace374d35ef913399
Shawn Law <shawnlaw6669@gmail.com>
2025-05-30 14:14:31 +0800

    get movie details

--- api/movies.ts
+++ api/movies.ts
@@ -1,11 +1,16 @@
+const JWT_TOKEN = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTI5MzAyNjk2YzFmNTY1NmNmNGIxODc4Mjk3ODQzOSIsIm5iZiI6MTYzMzkzODgzOC41NjA5OTk5LCJzdWIiOiI2MTYzZWQ5NmNlZTQ4MTAwNjJlOGIxYzAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fWCPCONvc6Ux3dooODvicGCstc6vjnkxROlmw_LBxY4`
+const BASE_URL = `https://api.themoviedb.org/3/`
+
+const headers = {
+  accept: 'application/json',
+  Authorization: `Bearer ${JWT_TOKEN}`
+}
+
 export const getTopRatedMovies = async () => {
-  const url = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+  const url = `${BASE_URL}movie/top_rated?language=en-US&page=1`;
   const options = {
     method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNTI5MzAyNjk2YzFmNTY1NmNmNGIxODc4Mjk3ODQzOSIsIm5iZiI6MTYzMzkzODgzOC41NjA5OTk5LCJzdWIiOiI2MTYzZWQ5NmNlZTQ4MTAwNjJlOGIxYzAiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fWCPCONvc6Ux3dooODvicGCstc6vjnkxROlmw_LBxY4'
-    }
+    headers
   };
 
   try {
@@ -14,6 +19,22 @@ export const getTopRatedMovies = async () => {
     // console.log(json.results)
     return json.results
   } catch (error) {
-    console.log(error)
+    throw error
+  }
+}
+
+export const getMovieById = async (id: number) => {
+  const url = `${BASE_URL}movie/${id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers
+  };
+
+  try {
+    const response = await fetch(url, options)
+    const json = await response.json()
+    return json
+  } catch (error) {
+    throw error
   }
 }
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,11 +1,32 @@
+import { getMovieById } from '@/api/movies'
+import { useQuery } from '@tanstack/react-query'
 import { useLocalSearchParams } from 'expo-router'
-import { View, Text } from 'react-native'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 
 export default function MovieDetails() {
   const {id} = useLocalSearchParams()
+  const {data: movie, error, isLoading} = useQuery({ 
+    queryKey: ['movies', id], 
+    queryFn: () => getMovieById(id)
+  })
+
+  if(isLoading) {
+    return <ActivityIndicator />
+  }
+  if(error) {
+    return <Text>{error.message}</Text>
+  }
+
   return (
     <View>
-      <Text>Movie {id}</Text>
+      <Text style={styles.title}>{movie.title}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 20,
+    fontWeight: '600'
+  }
+})
